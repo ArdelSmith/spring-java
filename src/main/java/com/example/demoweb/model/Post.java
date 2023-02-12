2a9913f0a5be3381be2d6e4a42ec5f3e1765feb2
Ivan Danilyuk <werwetsad@gmail.com>
2023-02-12 22:14:56 +0500

    Bootstrap

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,9 +1,18 @@
 package com.example.demoweb.model;
 
+import java.util.Date;
+
 public class Post {
     private String text;
     private Integer likes;
-    public Post(String text) {this.text = text;}
+    private Date creationDate;
+    public Post(String text, Date date)
+    {
+        this.text = text;
+        this.creationDate = date;
+    }
+    public Date getCreationDate(){return this.creationDate;}
     public String getText(){return this.text;}
     public Integer getLikes() {return this.likes;}
+
 }
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -3,14 +3,15 @@ package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
+import java.util.Date;
 
 @Service
 public class PostService {
     public static ArrayList<Post> listAllPosts(){
         ArrayList<Post> list = new ArrayList<>();
-        Post f = new Post("first one");
-        Post s = new Post("second one");
-        Post t = new Post("third one");
+        Post f = new Post("first one", new Date());
+        Post s = new Post("second one", new Date());
+        Post t = new Post("third one", new Date());
         list.add(f);
         list.add(s);
         list.add(t);
