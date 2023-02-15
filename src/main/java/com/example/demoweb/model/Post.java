d77cd7422a164bd5711b15080893d06e7bf6f2cb
Ivan Danilyuk <werwetsad@gmail.com>
2023-02-15 21:06:24 +0500

    Проверочный коммит добавления лайков

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -3,16 +3,20 @@ package com.example.demoweb.model;
 import java.util.Date;
 
 public class Post {
+    private Long id;
     private String text;
     private Integer likes;
     private Date creationDate;
-    public Post(String text, Date date)
+    public Post(Long id, String text, Date date)
     {
+        this.id = id;
         this.text = text;
         this.creationDate = date;
     }
+    public void setLikes(Integer likes) {this.likes = likes;}
     public Date getCreationDate(){return this.creationDate;}
     public String getText(){return this.text;}
     public Integer getLikes() {return this.likes;}
+    public Long getId() {return this.id;}
 
 }
